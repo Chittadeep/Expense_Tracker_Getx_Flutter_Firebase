0a943c60b3fd5f60817cbac668a16d642a862e93
Chittadeep Intern <chittadeep@ibarts.in>
2025-06-14 03:00:16 +0530

    feat: update TransactionHistory to use GetBuilder for dynamic expense display

--- lib/views/history.dart
+++ lib/views/history.dart
@@ -1,10 +1,27 @@
+import 'package:expense_tracker/controller/expenseController.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class TransactionHistory extends StatelessWidget {
+class TransactionHistory extends GetView<ExpenseController> {
   const TransactionHistory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("This is the hstory screnn"),);
+    return GetBuilder<ExpenseController>(builder: (context) {
+      return controller.expenses.isNotEmpty
+          ? ListView.builder(
+              itemBuilder: (context, i) {
+                String expenseDescription = controller.expenses[i].description;
+                String expenseAmount = controller.expenses[i].amount.toString();
+                String expenseDate = controller.expenses[i].dateTime.toString();
+                return ListTile(
+                  title: Text(expenseDate),
+                  subtitle: Text(expenseAmount),
+                );
+              },
+              itemCount: controller.expenses.length,
+            )
+          : const Center(child: Text("No expenses added"));
+    });
   }
 }
